a529ad36517b292fe44e3fe8433cda687f9bc06b
EtherDEXchange <41330817+EtherDEXchange@users.noreply.github.com>
2018-07-24 11:03:19 +0200

    Update ETH node URL for production

--- src/services/web3/index.js
+++ src/services/web3/index.js
@@ -3,7 +3,7 @@ import Web3 from "web3";
 export default new Web3(
   new Web3.providers.WebsocketProvider(
     process.env.NODE_ENV === "production"
-      ? "ws://localhost:8546"
+      ? "wss://mainnet.infura.io/ws"
       : "wss://ropsten.infura.io/ws"
   )
 );
